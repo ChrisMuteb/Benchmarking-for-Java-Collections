140105b084721ed072bffc9437fd8b221ba515c4
ChrisMuteb <ibond.chris@gmail.com>
2022-12-16 23:01:23 +0200

    The generated key and its MD5 Hash value are insert, fetched and deleted from mysql database and the duration are recorded.

--- src/main/java/codejava/App.java
+++ src/main/java/codejava/App.java
@@ -3,6 +3,8 @@ package codejava;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+import java.sql.DriverManager;
 import java.util.Random;
 
 /**
@@ -11,12 +13,126 @@ import java.util.Random;
  */
 public class App 
 {
+    private static final int size = 100;//insert a million key and md5 hash value
+	private static long startTime, endTime, timeElapsed;
+	
+	private static String url = "jdbc:mysql://localhost:3306/manyrecords";
+	private static String username = "root";
+	private static String password = "65412";
+
+	private static String keyStr, valueStr;
+	private static PreparedStatement statement = null;
     public static void main( String[] args )
     {
-        System.out.println( "Key: " + getRandomString() );
-        System.out.println("Value: " + getMd5(getRandomString()));
-        // getRandomString();
+        mysqlFnInsert();
+		mysqlFnFetch();
+        mysqlFnDelete();
     }
+
+    public static long mysqlFnInsert() {
+        int rows = 0;
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database");
+            startTime = System.currentTimeMillis();
+            for(int i = 0; i < size; i++) {
+                String sql = "INSERT INTO many (keyStr, valueStr) VALUES(?, ?)";
+                statement = connection.prepareStatement(sql);
+                keyStr = getRandomString();
+                valueStr = getMd5(keyStr);
+                statement.setString(1, keyStr);
+                statement.setString(2, valueStr);
+                rows = statement.executeUpdate();
+            }
+            endTime = System.currentTimeMillis();
+            timeElapsed = endTime - startTime;
+            if(rows > 0) {
+                System.out.println(" row has been inserted.");
+            }
+            System.out.println("Time to insert into mysql db: " + timeElapsed + "ms");
+            
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Oops, error!");
+            e.printStackTrace();
+        }
+        
+        return timeElapsed;
+    }
+
+    public static long mysqlFnDelete() {
+		int rows = 0;
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+				
+			System.out.println("Connected to the database");
+			
+			startTime = System.currentTimeMillis();
+			for(int i = 1; i <= size; i++) {
+				String sql = "DELETE FROM many WHERE id=?";
+				statement = connection.prepareStatement(sql);
+				statement.setInt(1, i);
+				
+				rows = statement.executeUpdate();
+				
+			}
+			endTime = System.currentTimeMillis();
+			timeElapsed = endTime - startTime;
+			
+				
+				if(rows > 0) {
+					System.out.println("rows has been deleted.");
+				}
+			
+			System.out.println("Execution time in milliseconds: " + timeElapsed);
+			connection.close();
+				
+		} catch (SQLException e) {
+				
+			System.out.println("Oops, error!");
+			e.printStackTrace();
+		}
+		
+		timeElapsed = endTime - startTime;
+		
+		return timeElapsed;
+	}
+	
+	
+	public static long mysqlFnFetch() {
+		int rows = 0;
+		String key = null, value = null;
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected to the database");
+			startTime = System.currentTimeMillis();
+			String sql = "SELECT * FROM many LIMIT 100";
+			statement = connection.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery(sql);
+				
+			while(rs.next()) {
+				key = rs.getString("keyStr");
+				value = rs.getString("valueStr");
+				System.out.println(key + " " + value);
+			}
+
+			endTime = System.currentTimeMillis();
+			timeElapsed = endTime - startTime;
+			if(rows > 0) {
+				System.out.println("rows has been selected.");
+			}
+			
+			System.out.println("Execution time in milliseconds: " + timeElapsed);
+			connection.close();
+				
+		} catch (SQLException e) {
+				
+			System.out.println("Oops, error!");
+			e.printStackTrace();
+		}
+		
+		return timeElapsed;
+	}
 
     public static String getRandomString() {
 		Random rand = new Random();
